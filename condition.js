0fbc55af45e327b2d1c222e4d21bb879fe656aa5
Elsaax <146921872+Elsaax@users.noreply.github.com>
2023-10-06 12:00:59 +0200

    Add files via upload

---
+++ condition.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let a = 4;
+let b = 4;
+let c = 3;
+
+switch(a){
+    case( a == b):
+        console.log("a est egal a b");
+        break;
+    case ( a == c):
+        console.log("a est egal a c");
+        break;
+    default:
+        console.log("a est egal a rien");
+
+}
